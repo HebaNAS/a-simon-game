1257736c7537d0629f7c5f9d34cf901f6a13d539
HebaNAS <heba_el_shimy@yahoo.com>
2017-05-02 10:28:47 +0400

    Fixing node server bug

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get('/dist/', function(req, res) {
 });
 
 app.listen(port, function() {
-	console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', port);
 });
